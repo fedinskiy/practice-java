4ee470813037970ab86221310eeb0431241dd43f
fedinskiy <fdudinskiy@gmail.com>
2017-02-15 11:45:55 +0300

    полноценная загрузка классов из jar

--- reflection/src/main/java/classloader/GitHubClasLoader.java
+++ reflection/src/main/java/classloader/GitHubClasLoader.java
@@ -1,11 +1,11 @@
 package classloader;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Hashtable;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Created by fedinskiy on 15.02.17.
@@ -14,7 +14,8 @@ public class GitHubClasLoader extends ClassLoader {
     private Hashtable classes = new Hashtable();
     private final String gitHubClassPath = "https://raw.githubusercontent" +
             ".com/jaowl/practice-java/master/tmp/";
-    private final String classFileExtension=".class";
+    private final String jarName = "place.jar";
+    private final String classFileExtension = ".class";
 
     public GitHubClasLoader(ClassLoader parent) {
         super(parent);
@@ -45,14 +46,17 @@ public class GitHubClasLoader extends ClassLoader {
 
         try {
 
-            classURL = new URL(gitHubClassPath + className+classFileExtension);
+            classURL = new URL(gitHubClassPath+ jarName);
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
 
         InputStream is = null;
         try {
-            is = classURL.openStream();
+            JarFile jar = downloadJAR(classURL);
+            JarEntry entry = jar.getJarEntry(className + classFileExtension);
+            is = jar.getInputStream(entry);
             ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
             int nextValue = 0;
 
@@ -66,12 +70,28 @@ public class GitHubClasLoader extends ClassLoader {
             e.printStackTrace();
         }
 
-
-
         result = defineClass(className, classByte, 0, classByte.length, null);
         classes.put(className, result);
         return result;
     }
 
+    private JarFile downloadJAR(URL jarURL) throws IOException {
+        InputStream is = null;
+        FileOutputStream jarWriter = new FileOutputStream(jarName);
+        JarFile jar = null;
+
+        is = jarURL.openStream();
+        int nextValue = 0;
+
+        nextValue = is.read();
+        while (-1 != nextValue) {
+            jarWriter.write(nextValue);
+            nextValue = is.read();
+        }
+
+        jarWriter.close();
+        jar = new JarFile(new File(jarName));
+        return jar;
+    }
 
 }
